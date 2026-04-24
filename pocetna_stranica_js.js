6119546b6ca068fabc499e0d447d45aa41769910
viktoriap7 <pavlicevicviktoria@gmail.com>
2026-04-24 22:45:41 +0200

    nav animacije

--- pocetna_stranica_js.js
+++ pocetna_stranica_js.js
@@ -1,12 +1,16 @@
-const nav_padajuci_meni=document.querySelector(".nav_padajuci_meni")
+const nav_padajuci_meni=document.querySelector(".nav_padajuci_meni");
+const hamburger_dugme=document.querySelector(".hamburger_dugme");
 
+hamburger_dugme.addEventListener("click",()=>{
+    nav_padajuci_meni.classList.toggle("open");
+});/* 
 function otvori_nav_padajuci_meni(){
 /*     provjerava da li je padajuci meni otvoren ili ne,
     pa ga mijenja u obrnuto, funkcija povezana sa dugmetom hamburger */
-    if(nav_padajuci_meni.style.display === "inline"){
+ /*    if(nav_padajuci_meni.style.display === "inline"){
         nav_padajuci_meni.style.display="none";
     }
     else{
         nav_padajuci_meni.style.display="inline";
     }
-}
+} */ 
