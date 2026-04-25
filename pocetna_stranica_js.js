f60b7db454eeeaa326f8090e5a7ee58fed8812bc
viktoriap7 <pavlicevicviktoria@gmail.com>
2026-04-25 19:55:17 +0200

    nav animacije zavrseno

--- pocetna_stranica_js.js
+++ pocetna_stranica_js.js
@@ -1,9 +1,30 @@
 const nav_padajuci_meni=document.querySelector(".nav_padajuci_meni");
 const hamburger_dugme=document.querySelector(".hamburger_dugme");
+let resizeTimer;
+
+window.addEventListener("resize",()=>{
+    /* pretvara sve elemente od body u elemente koji imaju
+    .resize klasu iznad njih, ovime se sprecava prikazivanje
+    tranzicija koje treba da se dese samo prilikom klika */
+    document.body.classList.add("resize");
+    /* svaki put kada se pokrene ovaj event listener tajmer
+    se resetuje */
+    clearTimeout(resizeTimer);
+    /* ako prođe 150ms bez ponovnog poziva, izvrsava se kod iz 
+    zagrade */
+    resizeTimer=setTimeout(()=>{
+        document.body.classList.remove("resize");
+    },150);
+    /* ova funkcija se momentalno primjenjuje na sve elemente od
+    body, samo u css treba napraviti .resize .element{} 
+    kako bi imala efekta */
+});
 
 hamburger_dugme.addEventListener("click",()=>{
     nav_padajuci_meni.classList.toggle("open");
-});/* 
+});
+
+/* 
 function otvori_nav_padajuci_meni(){
 /*     provjerava da li je padajuci meni otvoren ili ne,
     pa ga mijenja u obrnuto, funkcija povezana sa dugmetom hamburger */
