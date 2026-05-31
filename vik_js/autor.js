d11fedbff31b09717f99b3dcd42eaa06940e4384
viktoriap7 <pavlicevicviktoria@gmail.com>
2026-05-31 18:06:57 +0200

    odrađen res. dizajn za autor i autori (djelimično)

--- vik_js/autor.js
+++ vik_js/autor.js
@@ -2,6 +2,9 @@ let firebaseUrl="https://web-projekat-602fa-default-rtdb.firebaseio.com";
 let autori={};
 let knjige={};
 let autor={};
+let br_slike=0;
+
+const slika_con=document.getElementById("slika");
 async function preuzmi_autora(){
     const parametri=new URLSearchParams(window.location.search);
     let autor_id=parametri.get("id");
@@ -12,33 +15,32 @@ async function preuzmi_autora(){
     console.log(odg2.status);
 console.log(knjige);
     prikazi_autora();
+    console.log(typeof minus);
 }
 function prikazi_autora(){
     const ime_con=document.getElementById("ime");
-    ime_con.innerHTML=`${autor.ime}`;
+    ime_con.innerHTML=`Име: ${autor.ime}`;
     const prezime_con=document.getElementById("prezime");
-    prezime_con.innerHTML=`${autor.prezime}`;
+    prezime_con.innerHTML=`Презиме: ${autor.prezime}`;
     const datum_con=document.getElementById("datum");
-    datum_con.innerHTML=`${autor.datumRodjenja}`;
+    datum_con.innerHTML=`Датум рођенја: ${autor.datumRodjenja}`;
     const status_con=document.getElementById("status");
-    status_con.innerHTML=`${autor.status}`;
+    status_con.innerHTML=`Статус: ${autor.status}`;
     const nagrade_con=document.getElementById("nagrade");
-    nagrade_con.innerHTML=`${autor.brojOsvojenihNagrada}`;
+    nagrade_con.innerHTML=`Број освојених награда: ${autor.brojOsvojenihNagrada}`;
     const primjerci_con=document.getElementById("primjerci");
-    primjerci_con.innerHTML=`${autor.brojProdatihPrimeraka}`;
+    primjerci_con.innerHTML=`Број продатих примјеара: ${autor.brojProdatihPrimeraka}`;
     const kontakt_con=document.getElementById("kontakt");
-    kontakt_con.innerHTML=`${autor.kontaktTelefonMenadzera}`;
+    kontakt_con.innerHTML=`Контакт: ${autor.kontaktTelefonMenadzera}`;
 
-    const slika_con=document.getElementById("slika");
     let slike=autor.slike||[];
-    slika_con.innerHTML=`
-                <button id="minus"><</button>
-                <img src="${slike[0]}" alt="Нема доступне слике">
-                <button id="plus">></button>`;
-
+    console.log(autor.slike);
+    ucitaj_sliku();
     const bio_con=document.getElementById("bio");
     bio_con.innerHTML=`${autor.biografija}`;
-    const knjige_con=document.getElementById("knjige");
+    const knjige_con=document.getElementById("lista");
+        console.log(knjige_con);
+
     knjige_con.innerHTML='';
     for(let knjiga_id in knjige){
         let knjiga=knjige[knjiga_id];
@@ -49,4 +51,24 @@ function prikazi_autora(){
     }
     
 }
-document.addEventListener("DOMContentLoaded",preuzmi_autora);
+function ucitaj_sliku(){
+    slika_con.innerHTML=`
+                <button onclick="minus()"><</button>
+                <img src="${autor.slike[br_slike]}" alt="Нема доступне слике">
+                <button onclick="plus()">></button>`;
+
+}
+function minus(){
+    if(br_slike==0){
+        br_slike=autor.slike.length -1;
+    }else{
+        br_slike=br_slike-1;
+    }
+    console.log("oduzet, br slike"+br_slike);
+    ucitaj_sliku();
+}
+function plus(){
+    br_slike=(br_slike+1)%autor.slike.length;
+    console.log("dodat, br slike"+br_slike);
+    ucitaj_sliku();
+}
---
+++ vik_js/sve.js
@@ -0,0 +1,32 @@
+let resizeTimer;
+
+document.addEventListener("DOMContentLoaded",preuzmi_autora);
+function padajuci_meni(){
+    const padajuci_meni=document.querySelector(".padajuci_meni");
+    padajuci_meni.classList.toggle("open");
+}
+window.addEventListener("resize",()=>{
+    /* pretvara sve elemente od body u elemente koji imaju
+    .resize klasu iznad njih, ovime se sprecava prikazivanje
+    tranzicija koje treba da se dese samo prilikom klika */
+    document.body.classList.add("resize");
+    /* svaki put kada se pokrene ovaj event listener tajmer
+    se resetuje */
+    clearTimeout(resizeTimer);
+    /* ako prođe 150ms bez ponovnog poziva, izvrsava se kod iz 
+    zagrade */
+    resizeTimer=setTimeout(()=>{
+        document.body.classList.remove("resize");
+    },150);
+    /* ova funkcija se momentalno primjenjuje na sve elemente od
+    body, samo u css treba napraviti .resize .element{} 
+    kako bi imala efekta */
+});
+function dodjela_indeksa_padajuci_meni(){
+    let meni = document.querySelectorAll(".padajuci_meni li");
+
+    for(let i=0;i<meni.length;i++){
+        meni[i].style.setProperty("--i", i+1);
+    }
+}
+dodjela_indeksa_padajuci_meni();
